22f00ecc6a8b40bbd4e5b8659a717bd0139fb7d2
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-08-13 18:57:25 -0300

    implemented new module: Main
    module that acts as a combination of all modules

---
+++ modules/main/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+import lexer.Lexer
+import parser.Parser
+import interpreter.Interpreter
+
+fun main(args: Array<String>) {
+    val lexer = Lexer("println(\"hello world\")")
+    val tokens = lexer.lex()
+    val parser = Parser(tokens)
+    println(tokens)
+    parser.parse()
+}
