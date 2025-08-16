c58c1dc32076af35fd3f918c29e953c10c349250
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-08-16 17:29:49 -0300

    NoMatchingParenthesisException
    implemented and locally tested

---
+++ modules/common/src/main/kotlin/exception/NoMatchingParenthesisException.kt
@@ -0,0 +1,8 @@
+package exception
+
+public class NoMatchingParenthesisException(
+    message: String,
+    cause: Throwable? = null
+) : Exception(message, cause) {
+    constructor(cause: Throwable? = null) : this("No matching parenthesis found", cause)
+}
