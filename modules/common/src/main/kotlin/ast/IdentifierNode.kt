368242155a462d42a61bf371f3b72fc93b11f924
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-08-16 00:56:27 -0300

    composite ast idea

---
+++ modules/common/src/main/kotlin/ast/IdentifierNode.kt
@@ -0,0 +1,14 @@
+package ast
+
+import ast.abs.*
+
+data class IdentifierNode(
+    val name: String,
+    override val parent: AstInterface? = null
+) : AstInterface {
+    override val children: List<AstInterface> = emptyList()
+
+    override fun accept(visitor: AstVisitor) {
+        visitor.visitIdentifier(this)
+    }
+}
