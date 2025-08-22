0e2037dc5ce1fabc93d10d21f0ec77aef746bf7c
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-08-22 16:58:24 -0300

    test: add unit tests for Lexer string literal handling

---
+++ modules/lexer/src/test/kotlin/lexer/LexerStringLiteralTest.kt
@@ -0,0 +1,22 @@
+package lexer
+
+import exception.NoMatchingQuotationMarksException
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import token.StringLiteralToken
+
+class LexerStringLiteralTest {
+    @Test
+    fun testStringLiteral() {
+        val tokens = Lexer("\"hello world\"").lex()
+        assert(tokens.isNotEmpty()) { "Expected tokens to be generated for string literal" }
+        assert(tokens.any { it is StringLiteralToken }) { "Expected String Literal Token"}
+    }
+
+    @Test
+    fun testOpenStringLiteral() {
+        assertThrows(NoMatchingQuotationMarksException::class.java) {
+            (Lexer("\"hello world").lex())
+        }
+    }
+}
