c4aafd91b1137ccb3b0ccaf774c9cd0f1961570a
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-08-22 16:40:44 -0300

    feat: add NoMatchingQuotationMarksRule and exception for quotation mark validation

---
+++ modules/lexer/src/main/kotlin/lexer/syntax/rules/NoMatchingQuotationMarksRule.kt
@@ -0,0 +1,15 @@
+package lexer.syntax.rules
+
+import exception.NoMatchingQuotationMarksException
+import lexer.syntax.SyntaxRule
+
+class NoMatchingQuotationMarksRule: SyntaxRule {
+
+    override fun match(line: String, index: Int, row: Int): Exception? {
+        val singleQuotes = line.count { it == '\'' }
+        val doubleQuotes = line.count { it == '\"' }
+        if (singleQuotes % 2 == 0 && doubleQuotes % 2 == 0)
+            return NoMatchingQuotationMarksException("No matching quotation marks found at row $row, index $index")
+        return null
+    }
+}
