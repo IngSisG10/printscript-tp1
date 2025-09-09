c2e5b1634ead4e691db0ae27bcd31f5be750e03c
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-09-09 15:23:43 -0300

    refactor: enable function validation in FunctionNodeCreator and improve error messages in FunctionValidator

--- modules/parser/src/main/kotlin/parser/nodecreator/FunctionNodeCreator.kt
+++ modules/parser/src/main/kotlin/parser/nodecreator/FunctionNodeCreator.kt
@@ -5,15 +5,16 @@ import common.ast.FunctionNode
 import common.token.FunctionToken
 import common.token.abs.TokenInterface
 import parser.nodecreator.abs.AstNodeCreator
+import parser.nodecreator.validators.FunctionValidator
 
 class FunctionNodeCreator : AstNodeCreator {
     private val operationNodeCreator = OperationNodeCreator()
-//    private val functionValidator = FunctionValidator()
+    private val functionValidator = FunctionValidator()
 
     override fun matches(line: List<TokenInterface>): Boolean = line.isNotEmpty() && line[0] is FunctionToken
 
     override fun createAstNode(line: List<TokenInterface>): AstNode {
-//        functionValidator.validate(line)
+        functionValidator.validate(line)
         val variableName = (line[0] as FunctionToken).value
         val parenthesis = line.subList(1, line.size)
 
--- modules/parser/src/main/kotlin/parser/nodecreator/validators/FunctionValidator.kt
+++ modules/parser/src/main/kotlin/parser/nodecreator/validators/FunctionValidator.kt
@@ -7,18 +7,13 @@ import common.token.abs.TokenInterface
 import parser.nodecreator.validators.abs.ParenthesisValidator
 import parser.nodecreator.validators.abs.StructureValidator
 
-// TODO: chequear el parentesis este bien hecho
 class FunctionValidator :
     StructureValidator,
     ParenthesisValidator {
     override fun validate(line: List<TokenInterface>) {
-        if (line.size > 2) {
-            throw UnrecognizedLineException("Invalid function structure")
-        }
         if (line[1] !is OpenParenthesisToken || line[line.size - 1] !is CloseParenthesisToken) {
-            throw UnrecognizedLineException("Expected '(', got: ${line[1].name}")
-        }
-        if (!validateParenthesis(line)) {
+            throw UnrecognizedLineException("Expected '(' or ')', got: ${line[1].name}")
+        } else if (!validateParenthesis(line)) {
             throw UnrecognizedLineException("Invalid function structure")
         }
     }
