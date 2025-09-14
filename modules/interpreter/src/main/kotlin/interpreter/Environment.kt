15bce3e483eda8c460f903a394de62647585fb8a
fersantisi <fer.santisi@hotmail.com>
2025-09-14 18:55:45 -0300

    feat: format NumberValue to two decimal places in string representation

--- modules/interpreter/src/main/kotlin/interpreter/Environment.kt
+++ modules/interpreter/src/main/kotlin/interpreter/Environment.kt
@@ -34,7 +34,7 @@ data class NumberValue(
         if (value % 1.0 == 0.0) {
             value.toInt().toString()
         } else {
-            value.toString()
+            String.format("%.2f", value)
         }
 }
 
