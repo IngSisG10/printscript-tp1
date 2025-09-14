b5c62d4022cf79b2fbbd2be66b0a0ffb57741bae
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2025-09-14 19:19:27 -0300

    refactor: update linter rules to improve configuration and validation logic

--- modules/linter/src/main/kotlin/linter/rules/required/SnakeCaseRule.kt
+++ modules/linter/src/main/kotlin/linter/rules/required/SnakeCaseRule.kt
@@ -3,10 +3,21 @@ package linter.rules.required
 import common.exception.InvalidSnakeCaseException
 import common.token.VariableToken
 import common.token.abs.TokenInterface
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonPrimitive
 import linter.rules.abs.LinterRule
 
 class SnakeCaseRule : LinterRule {
-    override fun getName(): String = "snake_case_rule"
+    override fun applies(rules: Map<String, JsonElement>): Boolean {
+        for ((key, value) in rules) {
+            if (key == "identifier_format") {
+                if (value.jsonPrimitive.content == "camel case") {
+                    return true
+                }
+            }
+        }
+        return false
+    }
 
     override fun match(tokens: List<TokenInterface>): List<Throwable> {
         val list = mutableListOf<Throwable>()
